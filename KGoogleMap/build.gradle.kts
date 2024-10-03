5e4efd30d78b72636531ad0b281f5d5c7fc185c1
Michelle Raouf <michelle.raouf@52ndsolution.net>
2024-10-03 13:37:48 +0300

    Add readme and published

--- KGoogleMap/build.gradle.kts
+++ KGoogleMap/build.gradle.kts
@@ -41,7 +41,7 @@ tasks.withType<PublishToMavenRepository> {
 
 
 mavenPublishing {
-    coordinates("io.github.the-best-is-best", "kgoogle-map", "1.0.0-beta1")
+    coordinates("io.github.the-best-is-best", "kgoogle-map", "1.0.0-rc1")
 
     publishToMavenCentral(SonatypeHost.S01, automaticRelease = true)
     signAllPublications()
