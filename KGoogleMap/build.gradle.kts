2103bc3b009cf085fea2548f6418788f52f4ccf2
Michelle Raouf <michelle.raouf@52ndsolution.net>
2024-12-29 12:26:32 +0200

    Upgrade Java compatibility to version 17 in build configurations

--- KGoogleMap/build.gradle.kts
+++ KGoogleMap/build.gradle.kts
@@ -230,8 +230,8 @@ android {
         minSdk = 23
 
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
         }
         buildFeatures {
             //enables a Compose tooling support in the AndroidStudio
