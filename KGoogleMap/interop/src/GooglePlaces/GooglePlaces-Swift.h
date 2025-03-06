95decbcbec677b54c4335ba0a56036686d628ba2
Michelle Raouf <michelle.raouf@52ndsolution.net>
2025-03-06 14:15:49 +0200

    Update GooglePlaces.h header file paths for simulator and device architectures
    -   Modify the include paths for `GooglePlaces.h` in `GooglePlaces-Swift.h` to reflect the correct locations for simulator (now using `ios-arm64_x86_64-simulator`) and device (using `ios-arm64`) architectures.

--- KGoogleMap/interop/src/GooglePlaces/GooglePlaces-Swift.h
+++ KGoogleMap/interop/src/GooglePlaces/GooglePlaces-Swift.h
@@ -1,5 +1,5 @@
 #if TARGET_OS_SIMULATOR  // For iOS simulator architecture (x86_64 or arm64)
-#import "/Users/michelleraouf/Desktop/kmm/kgoogle-map/KGoogleMap/interop/src/GooglePlaces/ios-arm_x86_64-simulator/Headers/GooglePlaces/GooglePlaces.h"
+#import "/Users/michelleraouf/Desktop/kmm/kgoogle-map/KGoogleMap/interop/src/GooglePlaces/ios-arm64_x86_64-simulator/Headers/GooglePlaces/GooglePlaces.h"
 #elif TARGET_CPU_ARM64  // For real iOS device architecture (arm64)
 #import "/Users/michelleraouf/Desktop/kmm/kgoogle-map/KGoogleMap/interop/src/GooglePlaces/ios-arm64/Headers/GooglePlaces/GooglePlaces.h"
 #else  // Fallback case for other platforms (e.g., iOS 32-bit, etc.)
